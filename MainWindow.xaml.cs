04198c42ceb251890fd9745dbb2e64aed45845e5
ramza <ramzanmollaev7@gmail.com>
2024-04-16 23:25:04 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFUIKitProfessional.ClassFolder;
+using WPFUIKitProfessional.DataFolder;
+using WPFUIKitProfessional.Pages;
+using WPFUIKitProfessional.Pages.ManagerFolder;
+
+namespace WPFUIKitProfessional
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void rdHome_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void rdAnalytics_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void rdMessages_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void rdCollections_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void rdUsers_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(new ListAttendancePage());
+        }
+    }
+}
